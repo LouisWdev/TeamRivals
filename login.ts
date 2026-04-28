e2a23a42df1244b68270d265f860adaed6bd2696
Gene Nees <s172479@ap.be>
2026-04-28 08:00:52 +0000

    tijdelijk commit om mongodb te pullen

---
+++ login.ts
@@ -0,0 +1,18 @@
+import bcrypt from "bcrypt";
+
+export async function createInitialUser() {
+    if (await userCollection.countDocuments() > 0) {
+        return;
+    }
+    let email : string | undefined = process.env.ADMIN_EMAIL;
+    let password : string | undefined = process.env.ADMIN_PASSWORD;
+    if (email === undefined || password === undefined) {
+        throw new Error("ADMIN_EMAIL and ADMIN_PASSWORD must be set in environment");
+    }
+    await userCollection.insertOne({
+        email: email,
+        password: await bcrypt.hash(password, saltRounds),
+        role: "ADMIN"
+    });
+}
+
